1b920443cefb2ac4781b2a8c83e5bd8c2ee4223b
antoniojoao10 <73102148+antoniojoao10@users.noreply.github.com>
2022-02-04 22:28:58 +0000

    Fix

--- ExA1.cpp
+++ ExA1.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+inline bool exists (const std::string& name) {
+            struct stat buffer;   
+            return (stat (name.c_str(), &buffer) == 0); 
+}
+
 int main(int argc, char** argv) {
+  bool ext = exists(argv[1]);
+  if(!ext) throw invalid_argument("File does not exist");
   FCM fc;
   double res1 = fc.main(argv[1],atoi(argv[2]), (double)atoi(argv[3])/100 );
   
--- lang.cpp
+++ lang.cpp
@@ -1,15 +1,25 @@
 #include"FCM.h"
 
+
 using namespace std;
 
+inline bool exists (const std::string& name) {
+            struct stat buffer;   
+            return (stat (name.c_str(), &buffer) == 0); 
+}
+
 int main(int argc, char** argv) {
+  bool ext = exists(argv[1]);
+  if(!ext) throw invalid_argument("File does not exist");
+  ext = exists(argv[4]);
+  if(!ext) throw invalid_argument("File does not exist");
   FCM fc;
   double res1 = fc.main(argv[1],atoi(argv[2]), (double)atoi(argv[3])/100 );
 
   double res2 = fc.lang(argv[4],atoi(argv[2]), (double)atoi(argv[3])/100 );
   
   cout << "Entropy\t\t-->\t" << res1 << endl;
-  cout << "Entropy using table from previous\t-->\t" << res2 << endl;
+  cout << "Entropy using table from previous file\t-->\t" << res2 << endl;
   
   double bits_symbol = fc.nBits / fc.histMap.size() ;
   cout << "Estimated number of bits per symbol: " << bits_symbol << endl;
